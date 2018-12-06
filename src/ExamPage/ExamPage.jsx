f4940fb2feface89b8fadbde4839a1dbae69db6d
Abhi <abhishek.ctos@gmail.com>
2018-12-06 18:13:01 +0530

    5.1

--- src/ExamPage/ExamPage.jsx
+++ src/ExamPage/ExamPage.jsx
@@ -123,6 +123,41 @@ class ExamPage extends React.Component {
   <container>
       <center><h1>SBI PO Prelims Exam Analysis</h1></center>
       <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      <center><h2>SBI PO 2018 Exam Pattern</h2></center>
+      <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Section</th>
+      <th scope="col">No. of Qs</th>
+      <th scope="col">MArks</th>
+      <th scope="col">Time</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">English Language</th>
+      <td>30</td>
+      <td>100 marks</td>
+      <td>20 mins</td>
+    </tr>
+    <tr>
+      <th scope="row">Quantitative Aptitude</th>
+      <td>35</td>
+      <td></td>
+      <td>20 mins</td>
+    </tr>
+    <tr>
+      <th scope="row">Reasoning Ability</th>
+      <td colspan="2">35</td>
+      <td>20 mins</td>
+    </tr>
+    <tr>
+      <th scope="row">TOTAL</th>
+      <td colspan="2">100</td>
+      <td>1 hour</td>
+    </tr>
+  </tbody>
+</table>
       <center><h1>SBI PO 2018 Exam Dates & Schedule</h1></center>
       <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
       <center><h1>SBI PO 2018 Vacancy list</h1></center>
