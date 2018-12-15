9fa57d78ee79e5696c461340bb60eed96f7dacdc
Surya <skiran13@gmail.com>
2018-12-16 01:36:29 +0530

    View Question functionality added

--- src/ViewQuestion/ViewQuestion.jsx
+++ src/ViewQuestion/ViewQuestion.jsx
@@ -2,15 +2,43 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { userActions } from '../_actions'
+import config from 'config';
 
-
+ 
 class ViewQuestion extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+        examcode:'' ,body:''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
   commentDidMount () {
     this.props.dispatch(userActions.getAll())
   }
 
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+}
+
+  handleSubmit(e){
+    e.preventDefault();
+const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state)
+    }
+    fetch(`${config.apiUrl}/admin/api/viewquestions`, requestOptions).then(response => response.json())
+    .then(body => this.setState({body: body}));
+  }
+
   render () {
-    const { user, users } = this.props
+
+   
+    const { user, users} = this.props
+    const { examcode,body} = this.state
     return (
       <div className='jumbotron'>
         <div class="card">
@@ -18,31 +46,31 @@ class ViewQuestion extends React.Component {
   View Question </div>
   <div class="card-body">
     <blockquote class="blockquote mb-0">
-    <form>
+    <form onSubmit={this.handleSubmit} role='form'>
   <div class="form-group">
     <label for="exampleInputEmail1">Enter code</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter code"/>
+    <input className='form-control'
+                name='examcode'
+                type='text'
+                placeholder='examcode'
+                required='true'
+                value={examcode}
+                onChange={this.handleChange}/>
   </div>
+  <button type="submit" class="btn btn-success mt-4 block">Submit</button><br/>
   </form>
-      <div className="container">
-      <p>1. What is your favourite drink?</p>
-      <ul>
-  <li>Coffee</li>
-  <li>Tea</li>
-  <li>Halp lassi</li>
-  <li>Pull lassi</li>
-</ul>
-<p>2. What is your favourite song?</p>
-      <ul>
-  <li>KIKI</li>
-  <li>Good guy</li>
-  <li>hihihi</li>
-  <li>gopi trans bgm</li>
-</ul>
+      <div className="container mt-5">
+      <ol>
+      {  Object.keys(this.state.body).map(key => ( 
+         <li><p>{this.state.body[key].title}</p>
+        <ul>
+        {Object.keys(this.state.body[key].options).map(op => ( <li>{this.state.body[key].options[op].name}</li>))}
+  </ul></li>
+))}</ol>
     </div>
     </blockquote>
     
-    <button type="button" class="btn btn-success mt-4 block">Next</button>
+   
   </div>
 </div>
       </div>
