ce8f13b6edcb76ba1c95295eeba59d5c029ef963
Surya <skiran13@gmail.com>
2018-12-20 18:55:52 +0530

    validation UI

--- src/NewTest/NewTest.jsx
+++ src/NewTest/NewTest.jsx
@@ -123,7 +123,7 @@ class NewTest extends React.Component {
             />
             <div
               class='card mx-auto'
-              style={{
+              style={{'margin-bottom':'0px',
                 'border-bottom-left-radius': '10px',
                 'border-bottom-right-radius': '10px'
               }}
@@ -162,7 +162,7 @@ class NewTest extends React.Component {
             />
             <div
               class='card mx-auto'
-              style={{
+              style={{'margin-bottom':'0px',
                 'border-bottom-left-radius': '10px',
                 'border-bottom-right-radius': '10px'
               }}
